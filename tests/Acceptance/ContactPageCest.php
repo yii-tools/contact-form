4d29801f47dc16617b2e7da697fe935c9c791f32
Wilmer Arambula <terabytesoftw@gmail.com>
2023-02-02 18:53:21 -0300

    Add tests.

---
+++ tests/Acceptance/ContactPageCest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\ContactForm\Tests\Acceptance;
+
+use Yii\ContactForm\Tests\Support\AcceptanceTester;
+
+final class ContactPageCest
+{
+    public function contactPageWorks(AcceptanceTester $I)
+    {
+        $I->amGoingTo('go to the contact page');
+        $I->amOnPage('/contact');
+        $I->wantTo('ensure that contact page works');
+        $I->see('Contact');
+    }
+
+    public function contactFormCanBeSubmitted(AcceptanceTester $I)
+    {
+        $I->amGoingTo('go to the contact page');
+        $I->amOnPage('/contact');
+        $I->amGoingTo('submit contact form with correct data');
+        $I->fillField('#contactform-name', 'tester');
+        $I->fillField('#contactform-email', 'tester@example.com');
+        $I->fillField('#contactform-subject', 'test subject');
+        $I->fillField('#contactform-message', 'test content');
+
+        $I->click('send');
+    }
+}
